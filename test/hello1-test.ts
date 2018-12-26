6b4ea00a666921a5b9debbf09d788e0fa83215ce
freewind <nowindlee@gmail.com>
2018-12-26 16:29:00 +0800

    demo

---
+++ test/hello1-test.ts
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+
+describe('hello1', () => {
+  it('hello1', () => {
+    console.log('hello1');
+    expect(1 + 1).to.equal(2);
+  })
+});
---
+++ test/hello2-test.ts
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+
+describe('hello2', () => {
+  it('hello2', () => {
+    console.log('hello2');
+    expect(1 + 1).to.equal(2);
+  })
+});
